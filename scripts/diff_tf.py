4b9af47b6a744ff67bd6a54d10eab6414e7c5e38
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2020-12-22 10:14:01 +0200

    Update base width and change odom to be calculated from avg of encs

--- scripts/diff_tf.py
+++ scripts/diff_tf.py
@@ -105,8 +105,8 @@ class DiffTf:
         self.then = rospy.Time.now()
         
         # subscriptions
-        rospy.Subscriber("/rear_left/encoder", Int16, self.lwheelCallback, queue_size=1)
-        rospy.Subscriber("/rear_right/encoder", Int16, self.rwheelCallback, queue_size=1)
+        rospy.Subscriber("/lwheels", Int16, self.lwheelCallback, queue_size=1)
+        rospy.Subscriber("/rwheels", Int16, self.rwheelCallback, queue_size=1)
         self.odomPub = rospy.Publisher("odom", Odometry, queue_size=10)
         self.odomBroadcaster = TransformBroadcaster()
         
